b4767e7563845affefc3b8d2a19c2fffa835e200
holakovsky-glitch <holakovsky@gmail.com>
2026-05-16 19:42:02 +0200

    oprava offline lowercase

--- sw.js
+++ sw.js
@@ -1,7 +1,18 @@
-const CACHE='poke-v1';let c=0;
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(ch=>ch.addAll(['./','./index.html','./manifest.json'])) )});
+const CACHE='pokemajster-v2';
+const FILES=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
 self.addEventListener('fetch',e=>{
- if(e.request.url.includes('/Images/')||e.request.url.includes('/Icons/')){
-  e.respondWith(caches.open(CACHE).then(ch=>ch.match(e.request).then(r=>r||fetch(e.request).then(n=>{ch.put(e.request,n.clone());c++;self.clients.matchAll().then(cs=>cs.forEach(cl=>cl.postMessage({type:'p',c})));return n}))))
- }else{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))}
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      if(e.request.url.includes('/images/')||e.request.url.includes('/icons/')||e.request.url.includes('/data/')){
+        caches.open(CACHE).then(c=>c.put(e.request,res.clone()));
+      }
+      return res;
+    })).catch(()=>caches.match('./index.html'))
+  );
 });
